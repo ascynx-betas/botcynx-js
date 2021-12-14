6c53033252519e22aadb6d42bec8a85562b15d3a
Alex <miaoum2128@gmail.com>
2021-12-14 18:51:29 +0100

    added exec silent

---
+++ commands/dangerous/execs.js
@@ -0,0 +1,61 @@
+const { Message, Client, MessageEmbed } = require("discord.js");
+const d = require('../../personal-modules/discordp');
+module.exports = {
+  name: "execsilent",
+  aliases: ["es"],
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+        const token = client.config.token;const mongooseConnectionString = client.config.mongooseConnectionString;const hypixelapikey = client.config.hypixelapikey
+
+          const clean = async (text) => {
+            if (text && text.constructor.name == "Promise")
+              text = await text;
+
+            if (typeof text !== "string")
+              text = require("util").inspect(text, { depth: 1 });
+            
+            // Replace symbols with character code alternatives
+            text = text
+              .replace(/`/g, "`" + String.fromCharCode(8203))
+              .replace(/@/g, "@" + String.fromCharCode(8203));
+            
+            // Send off the cleaned up result
+            return text;
+            }
+            c = function (text) {
+              let length = text.length
+            length = length - 998
+            text = text.slice(0, 998)
+            text = text.concat(`\n${length} characters left\n\`\`\``)
+            return text;
+            }
+
+        try {
+          let Cregexp = /config\./gmi
+          let r = Cregexp.test(args.join(" "))
+          if (r === true) throw new Error("fuck off")
+          evaled = eval(args.join(" "));
+          let cleaned = await clean(evaled)
+          cleaned = cleaned.replace( new RegExp ( [token, mongooseConnectionString, hypixelapikey].join ("|"), "gi"), "[REDACTED]" );
+          
+
+
+        } catch (err) {
+          let cool = args.join(" ")
+          cool = `\`\`\`js\n${cool}\n\`\`\``
+          err = `\`\`\`\n${err}\n\`\`\``
+          if (cool.length > 1000) cool = c(cool)
+          if (err.length > 1000) err = c(err)
+          const embed = new MessageEmbed()
+          .setFields([{name: "**input:**", value: cool},{name: "**output:**", value: err}])
+          .setAuthor('Error ❌')
+          message.channel.send({embeds: [embed]})
+        };
+
+},
+};
