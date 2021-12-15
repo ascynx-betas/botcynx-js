fdd593b0775fbda60050294d5fcfdf2e162fba14
Alex <miaoum2128@gmail.com>
2021-12-15 09:52:57 +0100

    cleaner RoleUpdate

--- events/RoleUpdate.js
+++ events/RoleUpdate.js
@@ -13,13 +13,15 @@ client.on("guildMemberUpdate", async (oldMember, newMember) => {
     let removable = config[0].removable;
     let bypass = config[0].bypass;
     let logchannel = config[0].logchannel;
-    let orr = mp.compare(oldMember._roles, trigger);
-    let nrr = mp.compare(newMember._roles, trigger);
+
+    const ora = oldMember._roles;
+    const nra = newMember._roles;
+
+    let orr = mp.compare(ora, trigger);
+    let nrr = mp.compare(nra, trigger);
 
     // if oldMember doesn't have and newMember has a trigger role
     if (nrr === true && orr === false) {
-      let ora = oldMember._roles;
-      let nra = newMember._roles;
       let diff = nra.filter((x) => !ora.includes(x));
       return client.channels.cache
         .get(logchannel)
@@ -30,8 +32,6 @@ client.on("guildMemberUpdate", async (oldMember, newMember) => {
     }
     // if oldMember has and newMember doesn't have a trigger role
     if (orr === true && nrr === false) {
-      let ora = oldMember._roles;
-      let nra = newMember._roles;
       let diff = ora.filter((x) => !nra.includes(x));
       client.channels.cache
         .get(logchannel)
@@ -41,18 +41,17 @@ client.on("guildMemberUpdate", async (oldMember, newMember) => {
         });
       // fuse trigger into bypass then check for them bypass roles
       bypass = bypass.concat(trigger);
-      console.log(bypass);
       let e = mp.compare(nra, bypass);
       if (e !== false) return;
       // doesn't have any bypasses / triggers that can stop anymore :thumbsup:
       rd = nra.filter((x) => removable.includes(x));
-      console.log(rd);
       rd.forEach(function (removable) {
         newMember.roles.remove(removable);
         client.channels.cache
           .get(logchannel)
           .send({
             content: `removed <@&${removable}> from ${newMember.user.tag}`,
+            allowedMentions: { parse: [] },
           });
       });
     }
